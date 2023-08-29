e896f810be11ca33117071f923ee27d840f3a628
Rami <rami.zayat9@gmail.com>
2023-08-29 03:38:56 +0200

    #undef INADDR_NONE added for compatibility with arduino as component

--- cores/esp32/IPAddress.h
+++ cores/esp32/IPAddress.h
@@ -92,6 +92,6 @@ public:
 };
 
 // changed to extern because const declaration creates copies in BSS of INADDR_NONE for each CPP unit that includes it
-#undef IPADDR_NONE
+#undef INADDR_NONE
 extern IPAddress INADDR_NONE;
 #endif
